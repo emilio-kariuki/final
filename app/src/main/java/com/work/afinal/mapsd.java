7f3e903e7eee6adc56fde214dcbf9c042c2d40bc
emilio kariuki <70817006+emiliokariuki@users.noreply.github.com>
2021-12-09 10:59:59 +0300

    commit

---
+++ app/src/main/java/com/work/afinal/mapsd.java
@@ -0,0 +1,14 @@
+package com.work.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mapsd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapsd);
+    }
+}
